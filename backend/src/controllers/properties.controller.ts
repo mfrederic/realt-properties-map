3d81b16da1f2f7a70bdf713f87ce3455d9219a03
Frederic Maury <contact@frederic-maury.com>
2024-11-17 13:30:49 +0100

    fix: realt community URL domain

--- backend/src/controllers/properties.controller.ts
+++ backend/src/controllers/properties.controller.ts
@@ -16,7 +16,7 @@ export async function get(req: Request, res: Response, next: NextFunction): Prom
       return res.json(cached)
     }
 
-    const { data: properties } = await axios.get('https://dashboard.realt.community/api/properties')
+    const { data: properties } = await axios.get('https://dashboard.realtoken.community/api/properties')
     cacheService.set(PropertiesCacheKey, properties, cachedTime)
     return res.json(properties)
   } catch (err) {
