5d3444c5a9bf462c5185c69330d3272e131ab9a1
Frederic Maury <contact@frederic-maury.com>
2024-12-23 18:30:05 +0100

    fix: correct clustering option not applying

---
+++ frontend/src/components/Map/MapCluster.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import MarkerClusterGroup from 'react-leaflet-cluster';
+import { selectMarkerClustering } from '../../store/mapOptions/mapOptionsSelector';
+import { useProperties } from '../../hooks/useProperties';
+import { Markers } from './Markers';
+import { useAppSelector } from '../../hooks/useInitStore';
+
+export function MapCluster() {
+  const properties = useProperties();
+  const markerClustering = useAppSelector(selectMarkerClustering);
+  const [key, setKey] = useState(0);
+
+  useEffect(() => {
+    setKey(prev => prev + 1);
+  }, [markerClustering]);
+
+  return (
+    <MarkerClusterGroup
+      key={key}
+      disableClusteringAtZoom={markerClustering}
+      showCoverageOnHover={false}
+      maxClusterRadius={100}
+      zoomToBoundsOnClick={true}
+      spiderfyOnMaxZoom={false}
+      removeOutsideVisibleBounds={true}
+      animate={false}
+      chunkedLoading={true}
+      chunkInterval={100}
+      chunkDelay={50}
+      singleMarkerMode={false}
+    >
+      <Markers properties={properties} />
+    </MarkerClusterGroup>
+  );
+}
--- frontend/src/components/Map/MapWrapper.tsx
+++ frontend/src/components/Map/MapWrapper.tsx
@@ -1,19 +1,14 @@
 import './MapWrapper.scss';
 import { useAppSelector } from '../../hooks/useInitStore';
 import { MapContainer, TileLayer } from 'react-leaflet';
-import MarkerClusterGroup from 'react-leaflet-cluster';
 import { PropertyPanel } from '../PropertyPanel/PropertyPanel';
-import { MapEvents } from './MapEvents';
 import { selectedLatLng, selectedZoom } from '../../store/urlQuery/urlQuery.selector';
-import { useProperties } from '../../hooks/useProperties';
-import { Markers } from './Markers';
-import { selectMarkerClustering } from '../../store/mapOptions/mapOptionsSelector';
+import { MapCluster } from './MapCluster';
+import { MapEvents } from './MapEvents';
 
 export function MapWrapper() {
   const center = useAppSelector(selectedLatLng);
   const zoom = useAppSelector(selectedZoom);
-  const properties = useProperties();
-  const markerClustering = useAppSelector(selectMarkerClustering);
 
   return (
     <>
@@ -22,22 +17,8 @@ export function MapWrapper() {
         center={center}
         zoom={zoom}>
         <TileLayer url={`https://tile.openstreetmap.org/{z}/{x}/{y}.png`}/>
-        <MarkerClusterGroup
-          disableClusteringAtZoom={markerClustering}
-          showCoverageOnHover={false}
-          chunkedLoading={true}
-          maxClusterRadius={100}
-          zoomToBoundsOnClick={true}
-          spiderfyOnMaxZoom={false}
-          removeOutsideVisibleBounds={true}
-          animate={false}
-          chunkInterval={100}
-          chunkDelay={50}
-          singleMarkerMode={false}
-        >
-          <Markers properties={properties} />
-          <MapEvents />
-        </MarkerClusterGroup>
+        <MapCluster />
+        <MapEvents />
       </MapContainer>
       <PropertyPanel />
     </>
