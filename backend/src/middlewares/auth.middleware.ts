7a7c0b7016d16ebbb869cdbbc6848c506dc6b73e
Frederic Maury <contact@frederic-maury.com>
2024-12-25 15:40:22 +0100

    fix: auth api token handling

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -10,7 +10,7 @@ function hashToken(token: string): string {
 
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   const actual_token = process.env.API_TOKEN
-    ? hashToken(process.env.API_TOKEN)
+    ? process.env.API_TOKEN
     : '92f5b24048bd100fc7cb1dc770f0d72e1ae4300eb9e13f642304ee71e8515ef4';
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
@@ -19,7 +19,7 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
     return res.status(401).json({ message: 'No token provided' });
   }
 
-  if (token !== actual_token) {
+  if (hashToken(token) !== actual_token) {
     return res.status(403).json({ message: 'Invalid token' });
   }
 
